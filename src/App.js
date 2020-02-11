zoremulla/RJS5-TheIndex-Axios

Request data from API

src/App.js
@@ -1,13 +1,36 @@
 import React, { Component } from "react";
-
-// Data
-import authors from "./data";
+import axios from "axios";
 
 // Components
 import Sidebar from "./Sidebar";
+import Loading from "./Loading";
 import AuthorsList from "./AuthorsList";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      authors: [],
+      loading: true
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get("https://the-index-api.herokuapp.com/api/authors/")
+      .then(res => res.data)
+      .then(authors => this.setState({ authors, loading: false }))
+      .catch(err => console.error(err));
+  }
+
+  getContentView() {
+    if (this.state.loading) {
+      return <Loading />;
+    } else {
+      return <AuthorsList authors={this.state.authors} />;
+    }
+  }
+
   render() {
     return (
       <div id="app" className="container-fluid">
@@ -15,9 +38,7 @@ class App extends Component {
           <div className="col-2">
             <Sidebar />
           </div>
-          <div className="content col-10">
-            <AuthorsList authors={authors} />
-          </div>
+          <div className="content col-10">{this.getContentView()}</div>
         </div>
       </div>
     );

sasa17/RJS4-TheIndex-Events-and-States

src/App.js
@@ -1,25 +1,48 @@
-import React from "react";
-
-// Data
-import authors from "./data";
+import React, { Component } from "react";
+import axios from "axios";
 
 // Components
 import Sidebar from "./Sidebar";
+import Loading from "./Loading";
 import AuthorList from "./AuthorList";
 
-function App() {
-  return (
-    <div id="app" className="container-fluid">
-      <div className="row">
-        <div className="col-2">
-          <Sidebar />
-        </div>
-        <div className="content col-10">
-          <AuthorList authors={authors} />
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      authors: [],
+      loading: true
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get("https://the-index-api.herokuapp.com/api/authors/")
+      .then(res => res.data)
+      .then(authors => this.setState({ authors, loading: false }))
+      .catch(err => console.error(err));
+  }
+
+  getContentView() {
+    if (this.state.loading) {
+      return <Loading />;
+    } else {
+      return <AuthorList authors={this.state.authors} />;
+    }
+  }
+
+  render() {
+    return (
+      <div id="app" className="container-fluid">
+        <div className="row">
+          <div className="col-2">
+            <Sidebar />
+          </div>
+          <div className="content col-10">{this.getContentView()}</div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;